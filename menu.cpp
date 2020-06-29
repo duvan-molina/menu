5794d4d35d8142c8458fe364afead59640fd5b1a
duvan molina <duvan074@gmail.com>
2020-06-29 12:01:07 -0500

    add the andres program

--- menu.cpp
+++ menu.cpp
@@ -7,6 +7,48 @@ int helloWorld() {
     return 0;
 }
 
+int average () {
+    int number [100], arrayNumber;
+    int sum = 0;
+    int average = 0;
+
+    cout<<"Digite el numero de elementos que va a tener su arreglo: ";
+    cin>>arrayNumber;
+
+    for(int i=0;i<arrayNumber;i++){
+        cout<< "Digite un numero: ";
+        cin>>number[i]; //se guarda los elementos que se digiten
+    }
+
+
+    //Vamos a mostrar lo elementos guardados
+    cout<<"Se mostrara la posicion donde se gravo cada numero: "<<endl;
+
+    for(int i=0;i<arrayNumber;i++){
+        cout<<i<<" -> " <<number[i]<<endl;
+    }
+
+
+    //Suma de los numeros digitados
+
+    for(int i=0;i<arrayNumber;i++){
+        sum = sum + number[i];
+    }
+
+    cout<<"La suma de los numero es: "<<sum<<endl;
+
+    //Promedio de los numeros digitados
+
+    for(int i=0;i<arrayNumber;i++){
+    average = sum / arrayNumber;
+
+    }
+
+    cout<<"El promedio de los numeros es: "<<average<<endl;
+
+    return 0;
+}
+
 int moonWeight() {
     int weight;
     float gravity = 9.8;
@@ -19,26 +61,35 @@ int moonWeight() {
 }
 
 int main(){
-
-//  menu principal
-    string program[2] = {"Hola mundo", "Peso en la luna"};
+    int opcion;
+    while(opcion) {
+    //  menu principal
+    string program[5] = {"Hola mundo", "Peso en la luna", "Promedio", "Programa de jorge",  "Salir"};
     int numberProgram;
+
     cout << "Eliga el programa que quiere ejecutar \n"<< endl;
-    for(int i = 0; i< 2; i++){
-        cout << program[i] << " " << i <<"\n" << endl;
+    for(int i = 0; i< 5; i++){
+        cout << program[i] << " -> " << i+1 <<"\n" << endl;
     }
 
     cout << "Digite el numero del programa ";
     cin >> numberProgram;
     cout << "\n";
 
-    if(numberProgram == 0){
-        helloWorld();
-    }else if (numberProgram == 1) {
-        moonWeight();
+    switch(numberProgram){
+        case 1 : helloWorld();
+        break;
+        case 2 : moonWeight();
+        break;
+        case 3:  average();
+        break;
+        case 4:  cout << "Programa de jorge";
+        break;
+        default : exit(0);
     }
 
     cout << "\n";
+    }
 // fin del menu principal
 
     return 0;
